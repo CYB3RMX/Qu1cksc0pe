c6e5dd8f9e508fc23136589045238c925cd41e6e
CYB3RMX <42123683+CYB3RMX@users.noreply.github.com>
2019-10-19 01:42:34 +0300

    Add files via upload

--- qu1cksc0pe.py
+++ qu1cksc0pe.py
@@ -21,17 +21,17 @@ def scope():
    target_file = sys.argv[1]
    regdict={
       "Registry": ["RegKeyOpen","RegSetValue","RegGetValue"],
-      "File": ["CreateFile","ReadFile","WriteFile","FindResource","LoadResource","FindFirstFile","FindNextFile","NtQueryDirectoryFile","CreateFileMapping","MapViewOfFile"],
+      "File": ["CreateFile","ReadFile","WriteFile","FindResource","LoadResource","FindFirstFile","FindNextFile","NtQueryDirectoryFile","CreateFileMapping","MapViewOfFile","GetTempPath","SetFileTime"],
       "Network": ["WSAStartup","WSAGetLastError","socket","recv","connect","getaddrinfo","accept","send","listen"],
       "Web": ["InternetOpen","InternetOpenURL","InternetConnect","InternetReadFile","InternetWriteFile","HTTPOpenRequest","HTTPSendRequest","HTTPQueryInfo","URLDownloadToFile"],
       "Keyboard/Keylogger": ["SetWindowsHook","CallNextHook","MapVirtualKey","GetKeyState","GetAsyncKeyState","GetForegroundWindow","AttachThreadInput"],
-      "Process": ["VirtualAlloc","VirtualProtect","OpenProcess","EnumProcesses","EnumProcessModules","CreateRemoteThread","WriteProcessMemory","AdjustTokenPrivileges","IsWow64Process","QueueUserAPC"],
+      "Process": ["CreateProcess","VirtualAlloc","VirtualProtect","OpenProcess","EnumProcesses","EnumProcessModules","CreateRemoteThread","WriteProcessMemory","AdjustTokenPrivileges","IsWow64Process","QueueUserAPC","NtSetInformationProcess"],
       "Dll": ["LoadLibrary","GetProcAddress","LdrLoadDll"],
       "Debugger Identifying": ["IsDebuggerPresent","CheckRemoteDebuggerPresent","FindWindow","GetTickCount","NtQueryInformationProcess","OutputDebugString"],
       "System Persistence": ["CreateService","ControlService"],
       "COM Object": ["OleInitialize","CoInitialize"],
       "Data Leakage": ["LsaEnumerateLogonSessions","SamIConnect","SamIGetPrivateData","SamQueryInformationUse","NetShareEnum","ReadProcessMemory","Toolhelp32ReadProcessMemory"],
-      "Other": ["CreateMutex","CreateProcess","ShellExecute","WinExec","System","CryptAcquireContext","EnableExecuteProtectionSupport","NtSetInformationProcess","GetSystemDefaultLangId","GetTempPath","SetFileTime","StartServiceCtrlDispatcher","IsNTAdmin","IsUserAnAdmin"]
+      "Other": ["CreateMutex","ShellExecute","WinExec","System","CryptAcquireContext","EnableExecuteProtectionSupport","GetSystemDefaultLangId","StartServiceCtrlDispatcher","IsNTAdmin","IsUserAnAdmin"]
    }
    for category in regdict:
        print("\n\u001b[96m[\u001b[91m+\u001b[96m]\u001b[0m Checking\u001b[92m {}\u001b[0m activites...\n".format(category))
