a1f80510e0a068d44c89d584c4447cddb3888a32
CYB3RMX <42123683+CYB3RMX@users.noreply.github.com>
2019-10-18 16:42:36 +0300

    Add files via upload

---
+++ grepper.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Colors
+cyan='\e[96m'
+red='\e[91m'
+green='\e[92m'
+default='\e[0m'
+yellow='\e[93m'
+
+# Arguments
+target=$1
+wordz=$2
+
+xxd $target | grep "${wordz}" &>/dev/null
+if [ $? -eq 0 ];then
+  echo -en "$red=>$default Found: ${green}$wordz$default\n"
+fi
