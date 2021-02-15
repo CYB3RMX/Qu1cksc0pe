90ebf8e485329a1d059690ccd0812be1a439dd4b
CYB3RMX <cyb3rmx0@gmail.com>
2021-02-15 03:17:05 +0300

    News 15/02/2021 03:17

--- Modules/languageDetect.py
+++ Modules/languageDetect.py
@@ -14,7 +14,7 @@ except:
     print("Error: >colorama< module not found.")
     sys.exit(1)
 
-# Getting name of the file for statistics
+# Getting name of the file for executable checker function
 fileName = str(sys.argv[1])
 
 # Colors
@@ -38,10 +38,11 @@ detector = {"Golang": ["GODEBUG", "runtime.goexit", "runtime.gopanic"],
             "Python": ["_PYI_PROCNAME", "Py_BuildValue", "Py_Initialize", "__main__", "pydata", "libpython3.9.so.1.0", "py_compile"],
             "Zig": ["ZIG_DEBUG_COLOR", "__zig_probe_stack", "__zig_return_error", "ZIG"],
             "C#": ["#GUID", "</requestedPrivileges>", "<security>", "mscoree.dll", "System.Runtime", "</assembly>", ".NET4.0E", "_CorExeMain"],
-            "C++": ["std::", "libstdc++.so.6"],
-            "C": ["__libc_start_main", "GLIBC_2.2.5", "libc.so.6"]
-}
+            "C++": ["std::", "libstdc++.so.6", "GLIBCXX_3.4.9", "CXXABI_1.3.9"],
+            "C": ["__libc_start_main", "GLIBC_2.2.5", "libc.so.6", "__cxa_finalize", ".text"]
+} # TODO: Look for better solutions instead of strings!!
 
+# This function scans special strings in binary files
 def LanguageDetect():
     print(f"{infoS} Performing language detection. Please wait!!")
 
@@ -57,21 +58,25 @@ def LanguageDetect():
         print(f"{errorS} Programming language couldn\'t detected :(\n")
         sys.exit(1)
 
-# Execution
-indmag = 0
-try:
-    magicNums = list(pr.magic_file(fileName))
-    for mag in range(0, len(magicNums)):
-        if magicNums[mag].confidence >= 0.4:
-            if "executable" in str(magicNums[mag].name) or "Executable" in str(magicNums[mag].name):
-                indmag += 1
-                LanguageDetect()
-            else:
-                pass
+# This function analyses if given file is an executable file
+def ExecutableCheck(fileName):
+    exe_indicator = 0
+    try:
+        magicNums = list(pr.magic_file(fileName))
+        for mag in range(0, len(magicNums)):
+            if magicNums[mag].confidence >= 0.4:
+                if "executable" in str(magicNums[mag].name) or "Executable" in str(magicNums[mag].name):
+                    exe_indicator += 1
+        if exe_indicator != 0:
+            return True
         else:
-            pass
-    if indmag == 0:
-        print(f"{errorS} Please scan executable files.\n")
-        sys.exit(1)
-except:
-    pass
+            return False
+    except:
+        pass
+
+# Execution
+if ExecutableCheck(fileName) == True:
+    LanguageDetect()
+else:
+    print(f"{errorS} Please scan executable files.\n")
+    sys.exit(1)
