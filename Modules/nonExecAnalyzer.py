0e3e2485e60aa3c4c970b80b5aecbd8979d4c9c8
CYB3RMX <cyb3rmx0@gmail.com>
2021-02-23 23:01:19 +0300

    News 23/02/2021 23:00

--- Modules/nonExecAnalyzer.py
+++ Modules/nonExecAnalyzer.py
@@ -27,6 +27,9 @@ except:
 # Checking for oletools
 try:
     from oletools.olevba import VBA_Parser
+    from oletools.crypto import is_encrypted
+    from oletools.oleid import OleID
+    from olefile import isOleFile
 except:
     print("Error: >oletools< module not found.")
     print("Try 'sudo -H pip3 install -U oletools' command.")
@@ -53,22 +56,65 @@ def MacroHunter(targetFile):
     answerTable.field_names = [f"{green}Threat Levels{white}", f"{green}Macros{white}", f"{green}Descriptions{white}"]
 
     print(f"\n{infoS} Looking for VBA Macros...")
-    fileData = open(targetFile, "rb").read()
-    vbaparser = VBA_Parser(targetFile, data=fileData)
-    if vbaparser.contains_macros == True:
+    try:
+        fileData = open(targetFile, "rb").read()
+        vbaparser = VBA_Parser(targetFile, fileData)
         macroList = list(vbaparser.analyze_macros())
-        for fi in range(0, len(macroList)):
-            if macroList[fi][0] == 'Suspicious':
-                answerTable.add_row([f"{yellow}{macroList[fi][0]}{white}", f"{macroList[fi][1]}", f"{macroList[fi][2]}"])
-            elif macroList[fi][0] == 'IOC':
-                answerTable.add_row([f"{magenta}{macroList[fi][0]}{white}", f"{macroList[fi][1]}", f"{macroList[fi][2]}"])
-            elif macroList[fi][0] == 'AutoExec':
-                answerTable.add_row([f"{red}{macroList[fi][0]}{white}", f"{macroList[fi][1]}", f"{macroList[fi][2]}"])
-            else:
-                answerTable.add_row([f"{macroList[fi][0]}", f"{macroList[fi][1]}", f"{macroList[fi][2]}"])
-        print(f"{answerTable}\n")
+        if vbaparser.contains_macros == True:
+            for fi in range(0, len(macroList)):
+                if macroList[fi][0] == 'Suspicious':
+                    if "(use option --deobf to deobfuscate)" in macroList[fi][2]:
+                        sanitized = f"{macroList[fi][2]}".replace("(use option --deobf to deobfuscate)", "")
+                        answerTable.add_row([f"{yellow}{macroList[fi][0]}{white}", f"{macroList[fi][1]}", f"{sanitized}"])
+                    elif "(option --decode to see all)" in macroList[fi][2]:
+                        sanitized = f"{macroList[fi][2]}".replace("(option --decode to see all)", "")
+                        answerTable.add_row([f"{yellow}{macroList[fi][0]}{white}", f"{macroList[fi][1]}", f"{sanitized}"])
+                    else:
+                        answerTable.add_row([f"{yellow}{macroList[fi][0]}{white}", f"{macroList[fi][1]}", f"{macroList[fi][2]}"])
+                elif macroList[fi][0] == 'IOC':
+                    answerTable.add_row([f"{magenta}{macroList[fi][0]}{white}", f"{macroList[fi][1]}", f"{macroList[fi][2]}"])
+                elif macroList[fi][0] == 'AutoExec':
+                    answerTable.add_row([f"{red}{macroList[fi][0]}{white}", f"{macroList[fi][1]}", f"{macroList[fi][2]}"])
+                else:
+                    answerTable.add_row([f"{macroList[fi][0]}", f"{macroList[fi][1]}", f"{macroList[fi][2]}"])
+            print(f"{answerTable}\n")
+        else:
+            print(f"{errorS} Not any VBA macros found.")
+    except:
+        print(f"{errorS} An error occured while parsing that file for macro scan.")
+
+# Gathering basic informations
+def BasicInfoGa(targetFile):
+    # Check for ole structures
+    if isOleFile(targetFile) == True:
+        print(f"{infoS} Ole File: {green}True{white}")
     else:
-        print(f"{errorS} Not any macros found.")
+        print(f"{infoS} Ole File: {red}False{white}")
+
+    # Check for encryption
+    if is_encrypted(targetFile) == True:
+        print(f"{infoS} Encrypted: {green}True{white}")
+    else:
+        print(f"{infoS} Encrypted: {red}False{white}")
+    
+    # VBA_MACRO scanner
+    vbascan = OleID(targetFile)
+    vbascan.check()
+    # Sanitizing the array
+    vba_params = []
+    for vb in vbascan.indicators:
+        vba_params.append(vb.id)
+
+    if "vba_macros" in vba_params:
+        for vb in vbascan.indicators:
+            if vb.id == "vba_macros":
+                if vb.value == True:
+                    print(f"{infoS} VBA Macros: {green}Found{white}")
+                    MacroHunter(targetFile)
+                else:
+                    print(f"{infoS} VBA Macros: {red}Not Found{white}")
+    else:
+        MacroHunter(targetFile)
 
 # A function that handles file types, extensions etc.
 def MagicParser(targetFile):
@@ -77,7 +123,7 @@ def MagicParser(targetFile):
 
     # Magic byte parsing
     resCounter = 0
-    resTable.field_names = [f"Extracted File Extensions", "Names", "Byte Matches", "Confidence"]
+    resTable.field_names = [f"File Extension", "Names", "Byte Matches", "Confidence"]
     resourceList = list(pr.magic_file(targetFile))
     for res in range(0, len(resourceList)):
         extrExt = str(resourceList[res].extension)
@@ -93,9 +139,10 @@ def MagicParser(targetFile):
         print(resTable)
 
 # Execution area
-if __name__ == '__main__':
-    MagicParser(targetFile)
-    try:
-        MacroHunter(targetFile)
-    except:
-        print(f"{errorS} File format not supported for macro scanning.")
+#try:
+BasicInfoGa(targetFile)
+print(f"\n{infoS} Performing magic number analysis...")
+MagicParser(targetFile)
+#except:
+    #print(f"{errorS} An error occured while analyzing that file.")
+    #sys.exit(1)
